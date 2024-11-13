696001624f70e28b4283978bdc5a5fbfd5265457
PwnEngine <pwnengine@tuta.io>
2024-11-12 23:57:49 -0800

    creating session table

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -44,6 +44,19 @@ await sql`
   )
 `;
 
+await sql`
+  CREATE TABLE IF NOT EXISTS "session" (
+    "sid" varchar NOT NULL COLLATE "default",
+    "sess" json NOT NULL,
+    "expire" timestamp(6) NOT NULL
+  )
+  WITH (OIDS=FALSE);
+
+  ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+  CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+`;
+
 const MAX_AMOUNT: number = 20;
 
 const app = express();
