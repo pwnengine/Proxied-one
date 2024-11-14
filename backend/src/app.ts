558105a3390c297091287997f86223ae8bd2d80e
PwnEngine <pwnengine@tuta.io>
2024-11-13 19:49:53 -0800

    vercel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -63,10 +63,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
   } else if(req.method === 'POST') {
+    res.setHeader('Access-Control-Allow-Origin', 'https://proxied-one.vercel.app');
     if(process.env.VERCEL === 'false') {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-    } else {
-      res.setHeader('Access-Control-Allow-Origin', 'https://proxied-one.vercel.app');
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     }
     
     res.setHeader('Access-Control-Allow-Methods', 'POST');
